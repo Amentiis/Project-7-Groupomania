2de87c180406d47d1447123ebe1f824f2994d33f
Amentiis <mat-leseur@hotmail.fr>
2022-09-27 23:33:19 +0200

    Last Commit

--- backend/controllers/modify.js
+++ backend/controllers/modify.js
@@ -17,22 +17,29 @@ exports.ModifyPost =  (req, res, next) => {
   .then(post => {
     if ((post.userId != req.auth.userId) && (req.auth.userId != '6332f583a71cc6f7749d2080')) {
           res.status(401).json({message: 'Not authorized'});
-      } else {
+      }else{
+        if(req.body.imageexiste){
+          Post.updateOne({ _id: req.params.id }, 
+            {...postObject,_id: req.params.id,})
+              .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+              .catch(error => res.status(400).json({ error }));
+        }else{c
           if (post.imageUrl){
-            const filename = post.imageUrl.split('/images/')[1];
-            fs.unlink(`images/${filename}`, () => {
-              Post.updateOne({ _id: req.params.id }, 
-                {...postObject,_id: req.params.id,imageUrl: isFile(),})
-                  .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-                  .catch(error => res.status(400).json({ error }));
-            });
-          }else{
+          const filename = post.imageUrl.split('/images/')[1];
+          fs.unlink(`images/${filename}`, () => {
             Post.updateOne({ _id: req.params.id }, 
               {...postObject,_id: req.params.id,imageUrl: isFile(),})
                 .then(() => res.status(200).json({ message: 'Objet modifié !'}))
                 .catch(error => res.status(400).json({ error }));
-          }
+            });
+        }else{
+          Post.updateOne({ _id: req.params.id }, 
+            {...postObject,_id: req.params.id,imageUrl: isFile(),})
+              .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+              .catch(error => res.status(400).json({ error }));
+            }
       }
+    }
   })
   .catch( error => {
       res.status(500).json({ error });
